9e221f85e459eef29b54419ee4de7fd6823ff5be
jilucev <jilucev7@gmail.com>
2018-08-16 15:31:14 -0700

    move release changelog

--- index.js
+++ index.js
@@ -1,29 +1,67 @@
+// LOG_LEVEL=trace npm start
+
+// context.log('Hodor, context log');
+//
+// app.log('Hodor, app log');
+// app.log.debug({data: 'here'}, 'Debugging with app log');
+
 /**
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
+ // The context object includes everything about the event that was triggered, and context.payload has the payload delivered by GitHub.
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!');
 
+  // Most events also include an "action".
+  // For example, the issues event has actions of
+  // assigned, unassigned, labeled, unlabeled, opened, edited, milestoned, demilestoned, closed, and reopened
   app.on('issues.opened', async context => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'jilucev', repo: 'ponyfriends', number: 123, body: 'Thanks for opening this issue!'}
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
 
     return context.github.issues.createComment(issueComment);
   })
 
   //cleans up the comments when the pr is deleted
+  // assigned, unassigned, labeled, unlabeled, opened, edited, closed, reopened, or synchronized
   app.on('pull_request.closed', async context => {
     const params = context.issue();
+    const {owner, repo, number} = params;
 
+    const result = await context.github.pullRequests.getCommits({owner, repo, number});
+    app.log('result', result);
+
+    // const tagParams = context.issue({owner: "jilucev", repo: "ponyfriends", tag: 'v0.0.1', message: 'AHOY', object: 'ca13d3c1775ff8750772191acd2d521184b3168e', type: 'commit'});
+    // app.log('tagParams', tagParams)
+    // const newTag = await context.github.gitdata.createTag(tagParams);
+
+    // context.github is an instance of the @octokit/rest Node.js module,
+    // which wraps the GitHub REST API and allows you to do almost anything programmatically that you can do through a web browser.
     const comments = await context.github.issues.getComments({owner, repo, number});
-    // app.log('comments.data', comments.data);
-    const gifComments = comments.data.filter(comment => comment.body.includes(`Go for it.`));
+    // app.log('comments', comments.data[0]);
+    app.log('comments DATA', comments.data);
 
-    return gifComments.forEach(gifComment => context.github.issues.deleteComment({owner, repo, comment_id: gifComment.id}));
+    return comments.data.forEach(comment => context.github.issues.deleteComment({owner, repo, comment_id: comment.id}));
+
+    // const gifComments = comments.data.filter(comment => comment.body.includes(`Go for it.`));
+    // return gifComments.forEach(gifComment => context.github.issues.deleteComment({owner, repo, comment_id: gifComment.id}));
+
+
+    // Note that creating a tag object does not create the reference that makes a tag in Git.
+    // If you want to create an annotated tag in Git, you have to do this call to create the tag object,
+    // and then create the refs/tags/[tag] reference.
+
+
+
   })
 
   app.on(['pull_request.opened', 'pull_request.reopened', 'pull_request.edited'], async context => {
+    // Return the owner, repo, and number params for making API requests against an issue or pull request.
+    // The object passed in will be merged with the repo params.
     const params = context.issue();
     const {owner, repo, number} = params;
 
